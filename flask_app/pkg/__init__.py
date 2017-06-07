6fe5de3461c9d70cc152f94ced7843818f4988d6
Kyle Schmidt <kyle.a.schmidt@gmail.com>
2017-06-07 09:44:36 -0400

    adding marshmallow class

--- flask_app/pkg/__init__.py
+++ flask_app/pkg/__init__.py
@@ -7,6 +7,7 @@ Application Initialization
 import os
 
 from flask import Flask  # type: ignore
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy  # type: ignore
 
 from .config import config, DevelopmentConfig
@@ -15,6 +16,7 @@ GITBRANCH = os.environ.get("GITBRANCH")
 app = Flask(__name__)
 app.config.from_object(config.get(GITBRANCH, DevelopmentConfig))
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 import pkg.models.users
 from pkg.api import api
 app.register_blueprint(api, url_prefix="/api")
--- flask_app/pkg/api/users.py
+++ flask_app/pkg/api/users.py
@@ -5,8 +5,14 @@ Kyle Schmidt
 Users API
 """
 from . import api
+from .. import ma
 from ..decorators.json import json
 from ..models import Users
+
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = Users
 
 
 @api.route("/users")
@@ -15,4 +21,5 @@ def get_users():
     """
     Get all users from the database
     """
-    return {"data": [record for record in Users.query.all()]}
+    user_schema = UserSchema()
+    return {"data": [user_schema.dump(user).data for user in Users.query.all()]}
