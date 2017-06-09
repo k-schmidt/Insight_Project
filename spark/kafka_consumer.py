3271f1dcf672d213a8647aa7d266970f68a24cc9
Ubuntu <ubuntu@ip-172-31-74-189.ec2.internal>
2017-06-09 00:52:05 +0000

    uncommenting and testing spark stream

--- spark/kafka_consumer.py
+++ spark/kafka_consumer.py
@@ -9,40 +9,23 @@ from pyspark.streaming.kafka import KafkaUtils
 sc = SparkContext(appName="stream_direct")
 ssc = StreamingContext(sc, 40)
 
-inputData = [[1, 2, 3],
-             [0],
-             [4, 4, 4],
-             [0, 0, 0, 25],
-             [1, -1, 10]]
+topics = ["create-user"]
+socket_format = "{}:{}"
+first = ["ec2-34-225-192-104.compute-1.amazonaws.com",
+         "ec2-34-197-55-50.compute-1.amazonaws.com",
+         "ec2-52-3-102-216.compute-1.amazonaws.com"]
 
-rddQueue = []
-for datum in inputData:
-    rddQueue += [ssc.sparkContext.parallelize(datum)]
+kafka_server_list = [socket_format.format(v, "9092") for v in first]
+bootstrap_servers = ",".join(kafka_server_list)
 
-inputStream = ssc.queueStream(rddQueue)
-inputStream.reduce(sum).pprint()
+
+
+create_user_stream = KafkaUtils.createDirectStream(ssc,
+                                                   topics,
+                                                   {"metadata.broker.list": bootstrap_servers})
+
+create_user_stream.pprint()
+#create_user_stream.saveAsTextFiles("s3a://insight-ks-test/create_user_stream/")
 
 ssc.start()
-time.sleep(5)
-ssc.stop(stopSparkContext=True, stopGraceFully=True)
-
-# topics = ["create-user"]
-# socket_format = "{}:{}"
-# first = ["ec2-52-44-117-247.compute-1.amazonaws.com",
-#          "ec2-52-200-2-182.compute-1.amazonaws.com",
-#          "ec2-34-198-26-209.compute-1.amazonaws.com"]
-
-# kafka_server_list = [socket_format.format(v, "9092") for v in first]
-# bootstrap_servers = ",".join(kafka_server_list)
-
-
-
-# create_user_stream = KafkaUtils.createDirectStream(stream,
-#                                                    topics,
-#                                                    {"metadata.broker.list": bootstrap_servers})
-
-# create_user_stream.pprint()
-# #create_user_stream.saveAsTextFiles("s3a://insight-ks-test/create_user_stream/")
-
-# stream.start()
-# stream.awaitTermination()
+ssc.awaitTermination()
