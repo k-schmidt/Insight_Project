57ca6241b4b90d159b99eacec3430f11ace7de9f
Ubuntu <ubuntu@ip-172-31-74-189.ec2.internal>
2017-06-10 20:15:02 +0000

    refactoring kafka spark consumer

--- spark/kafka_consumer.py
+++ spark/kafka_consumer.py
@@ -1,31 +1,46 @@
+from datetime import datetime
 import json
 import time
 
-from pyspark.sql import SQLContext
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import SQLContext
+
+from config_secure import ZkQuorum
 
 sc = SparkContext(appName="stream_direct")
 ssc = StreamingContext(sc, 40)
-
-topics = ["create-user"]
-socket_format = "{}:{}"
-first = ["ec2-34-225-192-104.compute-1.amazonaws.com",
-         "ec2-34-197-55-50.compute-1.amazonaws.com",
-         "ec2-52-3-102-216.compute-1.amazonaws.com"]
-
-kafka_server_list = [socket_format.format(v, "9092") for v in first]
-bootstrap_servers = ",".join(kafka_server_list)
+topics = {"create-user": 1}
 
 
+def create_stream(spark_stream_context,
+                  zk_quorum,
+                  group_name,
+                  topic_dict):
+    return KafkaUtils.createStream(spark_stream_context,
+                                   ",".join(zk_quorum),
+                                   group_name,
+                                   topic_dict)
 
-create_user_stream = KafkaUtils.createDirectStream(ssc,
-                                                   topics,
-                                                   {"metadata.broker.list": bootstrap_servers})
+def timestamp_to_datetime(user_dict):
+    datetime_obj = datetime.strptime(user_dict["created_time"], ("%Y-%m-%d %H:%M:%S.%f"))
+    user_dict["created_time"] = datetime_obj
+    return user_dict
 
-create_user_stream.pprint()
-#create_user_stream.saveAsTextFiles("s3a://insight-ks-test/create_user_stream/")
 
-ssc.start()
-ssc.awaitTermination()
+def main():
+    create_user_stream = create_stream(ssc,
+                                       ZkQuorum,
+                                       "create-user-group",
+                                       topics)
+    create_user_stream.map(lambda tup: json.loads(tup[1]))\
+                      .map(lambda json_dict: timestamp_to_datetime(json_dict))\
+                      .map(lambda rdd: rdd.saveToCassandra("instabrand", "users"))
+    #create_user_stream.saveAsTextFiles("s3a://insight-ks-test/create_user_stream/")
+
+
+if __name__ == "__main__":
+    main()
+    ssc.start()
+    ssc.awaitTermination()
