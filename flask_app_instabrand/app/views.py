bcf948ef13200b8534b0c9bb345b0167dec8a261
Kyle Schmidt <kyle.a.schmidt@gmail.com>
2017-06-19 01:32:05 -0400

    init upload functionality

--- flask_app_instabrand/app/views.py
+++ flask_app_instabrand/app/views.py
@@ -1,4 +1,9 @@
-from flask import render_template, url_for
+from datetime import datetime
+
+import boto3
+from flask import render_template, url_for, request
+from kafka.client import SimpleClient
+from kafka.producer import KeyedProducer
 
 import app
 from app import helper_methods
@@ -9,48 +14,57 @@ def user_photos(username):
     timeline = list(helper_methods.get_user_timeline(username, app.session))
 
     templateData = {
-            'size' : "big",  # request.args.get('size','thumb'),
-            'media' : timeline  # recent_media
+        'size' : "big",  # request.args.get('size','thumb'),
+        'media' : timeline,  # recent_media
+        "username": username
     }
 
     return render_template('base.html', **templateData)
 
 
-# Redirect users to Instagram for login
-@app.app.route('/connect')
-def main():
-    pass  #return redirect(url)
-
-
-# # Instagram will redirect users back to this route after successfully logging in
-# @app.route('/instagram_callback')
-# def instagram_callback():
-
-#         code = request.args.get('code')
-
-#         if code:
-
-#                 access_token, user = api.exchange_code_for_access_token(code)
-#                 if not access_token:
-#                         return 'Could not get access token'
-
-#                 app.logger.debug('got an access token')
-#                 app.logger.debug(access_token)
-
-#                 # Sessions are used to keep this data
-#                 session['instagram_access_token'] = access_token
-#                 session['instagram_user'] = user
-
-#                 return redirect('/') # redirect back to main page
-
-#         else:
-#                 return "Uhoh no code provided"
-
-
-
 @app.app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+
+@app.app.route("/handle-post/<username>", methods=["POST"])
+def user_upload(username):
+    username = request.form["username"]
+    tags = request.form["tags"].split(",")
+    upload_file = request.form["filename"]
+    date_today = datetime.today().strftime("%Y-%m-%d %H%M%S")
+
+    s3_bucket = "instabrand-assets"
+    s3_name = f"{username}/{date_today}"
+
+    boto3.resource('s3').meta.client.upload_file(upload_file,
+                                                 s3_bucket,
+                                                 s3_name)
+    url = boto3.client('s3').generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            "Bucket": s3_bucket,
+            "Key": s3_name
+        }
+    )
+    print(url)
+
+    kafka_message = {
+        "username": username,
+        "tags": tags,
+        "photo_link": url,
+        "created_time": date_today,
+        "latitude": "40.7128",
+        "longitude": "74.0059",
+        "event": "photo-upload"
+    }
+
+    simple_client = SimpleClient(servers)
+    producer = KeyedProducer(simple_client)
+    producer.send_messages("photo-upload",
+                           bytes(username, "utf-8"),
+                           json.dumps(kafka_message).encode("utf-8"))
+    return redirect(url_for('user_photos', username=username))
 
 
 # This is a jinja custom filter
