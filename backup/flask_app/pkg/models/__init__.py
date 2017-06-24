229deb8f95ec4110437ee1c5c6fe68169515df12
Kyle Schmidt <kyle.a.schmidt@gmail.com>
2017-06-23 22:43:18 -0400

    deleting dead code

--- backup/flask_app/pkg/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .users import Users
-from .comments import Comments
-from .photos import Photos
-from .tags import Tags
-from .likes import Likes
-# from .followers import Followers
-from .match_photos_tags import MatchPhotosTags
